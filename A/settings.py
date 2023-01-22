4faca8f13fbb1ed15ebdeba579d532d46a43d055
alireza-fa <alirezafeyze44@gmail.com>
2023-01-22 12:05:43 +0330

    fixed bug

--- A/settings.py
+++ A/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-edl!y0--ninz(n#bq+%m&samv162vonn#d^29=ckz&%g+h4ut(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
